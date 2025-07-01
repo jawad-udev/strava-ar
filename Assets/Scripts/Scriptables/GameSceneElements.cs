fbedcdb920c4e741274fbcfcc9da73e041c70e3e
Jawad Ahmad <jaawad.afzal@gmail.com>
2025-07-01 14:28:25 +0500

    first commit

---
+++ Assets/Scripts/Scriptables/GameSceneElements.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "GameSceneElements", menuName = "ScriptableObjects/GameSceneElements", order = 2)]
+public class GameSceneElements : ScriptableObject
+{
+    [Header("Canvas")]
+    public Canvas gameSceneCanvasPrefab;
+
+    [Header("Game Play Prefabs")]
+    public GameManager gameplayManager;
+}
